cd6d1cc25ec831433d9b97800b71bd35c02708b4
Mert Aksoy <mrtaksy99@gmail.com>
2023-03-26 15:48:27 +0300

    TechnoMarketApp
    Future Builder Method

---
+++ apple_shop/lib/Products.dart
@@ -0,0 +1,8 @@
+class Products{
+  int product_id;
+  String product_name;
+  String product_images_name;
+  double product_cost;
+
+  Products(this.product_id, this.product_name, this.product_images_name, this.product_cost);
+}
